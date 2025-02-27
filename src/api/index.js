4a3e6505088f0e85647f1a35d5e7a931f61eab8c
Usama Ahmad <16usama.ahmad@gmail.com>
2025-02-27 16:15:18 +0100

    Updated website

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import {useEffect,useState}from 'react';
 export const getPosts = async()=>{
     const response2 = await fetch("http://webshop-dev-env.eba-v7q5pntk.eu-central-1.elasticbeanstalk.com/api/products",{
         
-        method:"GET",
+        // method:"GET",
     });
    
     let data= await response2.json();
